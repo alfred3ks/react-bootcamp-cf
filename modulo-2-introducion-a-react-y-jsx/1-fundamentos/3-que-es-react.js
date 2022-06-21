0ad8bd65fe4c88aba2fae3c21954195d47d84a04
Alfredo Sánchez <alfred3ks@gmail.com>
2022-06-21 14:14:02 +0200

    Que es React.

---
+++ modulo-2-introducion-a-react-y-jsx/1-fundamentos/3-que-es-react.js
@@ -0,0 +1,20 @@
+/*
+
+¿Que es react? ¿Que problemas resuelve?
+
+React es una biblioteca de JS. Una biblioteca es una herramienta que resuelve problemas complejos para nosotros.
+
+En web tenemos algo que es el DOM, es un arbol.
+DOM lo que hace es mostrar distintas cosas y el navegador mira el DOM y este dibija cosas que muestra el DOM.
+
+Nosotros le decimos al dom que es lo que debe mostrar en una situacion dada.
+
+Problemas que resulve react:
+- Intermediario entre desarrolladores y el DOM, Se coloca entre el DOM y el desarrollador.
+
+- Componentes, Tengo un problema complejo? lo divido, la reautilizacion brilla, podemos crear un componente con otros componentes adentro independencia.
+
+- Declarativo, Es una manera de programar.
+
+
+*/
