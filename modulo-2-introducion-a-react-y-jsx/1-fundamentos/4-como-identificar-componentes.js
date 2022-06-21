8d421c15a4095424fa080240835a48a3d6c896e0
Alfredo Sánchez <alfred3ks@gmail.com>
2022-06-21 14:29:59 +0200

    Como identificar componentes y virtualDOM.

---
+++ modulo-2-introducion-a-react-y-jsx/1-fundamentos/4-como-identificar-componentes.js
@@ -0,0 +1,10 @@
+/*
+
+Los componentes: Como identificarlos?
+
+Basicamente son porciones de una vista que sean repetidas. Eso lo podemos crear como un componente.
+
+Los botones pueden ser un componente.
+Por ejemplo twitter su timelaps es un conjunto de componentes, ese es el ejemplo que pone.
+
+*/
---
+++ modulo-2-introducion-a-react-y-jsx/1-fundamentos/5-virtualDOM.js
@@ -0,0 +1,12 @@
+/*
+
+Virtual DOM:
+
+En el navegador tenemos el DOM, el arbol.
+En las paginas web tenemos que realizar los cambion en el DOM. Esto cambios son muy costosos a medida que nuestro DOM se va volviendo mas grande y complejo.
+
+Con React esto se soluciona, ya que este implementa un DOM virtual: El virtual DOM.
+Es una representacion del DOM que guardamos una copia en la memoria, posibilita que react sea declarativo, yo le digo a react en que estado quiero que este la pagina y se hara cargo de llevar al DOM a ese estado con la menor cantidad de cambios posibles.
+
+
+*/
