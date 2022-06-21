a64489d571052780a9982986643ce5b704fe4f80
Alfredo Sánchez <alfred3ks@gmail.com>
2022-06-21 12:08:02 +0200

    Pros y contras de la programacion funcional.

---
+++ modulo-1-fundamentos-de-javascript/3-programacion-funcional/17-pros-y-contras-de-la-programacion-funcional.js
@@ -0,0 +1,17 @@
+/*
+
+Ventajas y desentajas de la programacion funcional:
+
+Ventajas:
+- Complejidad del codigo: Dividimos el problema en soluciones pequeñas que son mas faciles de mantener.
+- Pruebas unitarias: Una funcion pura es mas facil de testear.
+- Divison: Cada funcion esta isolada, por lo que es mas facil modificarla o rremplazarla sin alterar el sistema.
+- Expresividad del codigo: El codigo que resulta de un enfoque funcional puede ser en muchos casos ocasiones mas legible y expresivo.
+- Menos bugs o bugs mas facil de encontrar: aplicando conceptos como el de inmutabilidad nos aseguramos que las operaciones que realiza una funcion no termina por afectar las otras funciones.
+
+Desventajas:
+- Overhead al organizar el codigo: En muchas ocasiones tratar de respetar las recomendaciones del paradigma terminara por hacernos pensar dos veces donde colocar nuestros elementos.
+- Manejo de estructuras grandes: Copiar toda una estructura para no modificarla puede ser costoso en terminos de recursos.
+- State management: A diferencia de otros paradigmas como el de objetos, donde el manejo de estado es para del paradigma y de los objetos en programcion funcional es mecesario implementar alguna estrategia.
+
+*/
