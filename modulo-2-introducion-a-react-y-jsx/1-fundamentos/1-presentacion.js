53a7bf1f89b85ec5629935362d6cf258830cd84d
Alfredo Sánchez <alfred3ks@gmail.com>
2022-06-21 12:34:22 +0200

    Fundamentos de ReactJS y JSX, presentacion y historia de react.

---
+++ modulo-2-introducion-a-react-y-jsx/1-fundamentos/1-presentacion.js
@@ -0,0 +1,14 @@
+/*
+
+Comenzamos con ReactJS.
+
+Tutora : Daiana Szwimer -> @uhhdai
+
+Indice de lo que veremos:
+- Historia de React.
+- Virtual DOM,
+- Sintaxis de JSX,
+- Practica.
+
+
+*/
---
+++ modulo-2-introducion-a-react-y-jsx/1-fundamentos/2-historia-de-react.js
@@ -0,0 +1,10 @@
+/*
+
+La historia de React:
+
+Facebook invento react para su uso interno en 2011 y lo convirtio en codigo abierto en 2013.
+
+En 2015 React tuvo su primer version estable, empresas como Netflix, Airbnb empezaron a usarlo.
+
+
+*/
