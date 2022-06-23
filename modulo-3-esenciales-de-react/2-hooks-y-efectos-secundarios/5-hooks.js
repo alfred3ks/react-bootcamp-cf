b616523433574adb0e111e50f79685df3cca245c
Alfredo Sánchez <alfred3ks@gmail.com>
2022-06-23 17:59:07 +0200

    Los hooks, breve explicacion.

---
+++ modulo-3-esenciales-de-react/2-hooks-y-efectos-secundarios/5-hooks.js
@@ -0,0 +1,20 @@
+/*
+
+Los Hooks: Lo que hemos visto de useStare eso es un hook.
+
+Los hooks son como funciones que normalmente comienzan con la palabra 'use'.
+
+Nos permiten reutilizar codigo. Nosotros mismos podemos crear hooks personalizados. Entre algunas hooks mas usado tenemos:
+- useState(),
+- useEfect().
+
+Los hooks siguen unas reglas:
+- No pueden estar dentro de un consicional,
+- Se tienen que llamar en el top-level del componente,
+Solo se pueden invocar en function components (o en otros hooks).
+
+En la documentacion de react podemos ver sobre las reglas de los hooks:
+
+https://es.reactjs.org/docs/hooks-rules.html
+
+*/
