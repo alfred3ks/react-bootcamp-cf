8ac9fe7bf7e6aaeb4dc411fdfcab063efd86f0f8
Alfredo Sánchez <alfred3ks@gmail.com>
2022-07-08 17:35:35 +0200

    Componentes controlado o no controlados.

---
+++ modulo-3-esenciales-de-react/3-formularios-y-eventos/12-controlled-components.js
@@ -0,0 +1,11 @@
+/*
+
+Vamos a ver los componentes controlados y los no controlados:
+
+Los componentes controlados:
+Son aquellos componentes que pueden mantener su propio estado y ser actualizados con base en la informacion suministrada por el usuario. -> input, select, radio button. etc.
+
+Los componentes no controlados:
+Son aquellos componentes en los cuales a nivel de programacion no los podemos controlar, simplemente ellos gestionan su propio estado, hacen lo que tienen que hacer y solamente proveen una respuesta. -> un slider.
+
+*/
