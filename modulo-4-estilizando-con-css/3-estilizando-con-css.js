6e7c75ca831c37d80644a88c927d672191d6d158
Alfredo Sánchez <alfred3ks@gmail.com>
2022-07-12 01:42:14 +0200

    Estilizando con css.

---
+++ modulo-4-estilizando-con-css/3-estilizando-con-css.js
@@ -0,0 +1,18 @@
+/*
+
+Existen varias formas de estilizar los componentes en React:
+
+- Plain CSS,
+- CSS Modules,
+- CSS in JS,
+- Preprocessors(SASS, PostCSS, LESS, Tailwind CSS), etc.
+
+Dependera de cada proyecto y del equipo que trabaja con el.
+
+Nosotros para esto vamos implentar CSS en JS.
+- Styled Components,
+- Emotion,
+- JSS,
+- Stitches.
+
+*/
