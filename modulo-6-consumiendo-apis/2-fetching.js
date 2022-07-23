e11b745b528daba6283805b6fe2f148833eeef53
Alfredo Sánchez <alfred3ks@gmail.com>
2022-07-23 18:52:02 +0200

    Fetching.

---
+++ modulo-6-consumiendo-apis/2-fetching.js
@@ -0,0 +1,18 @@
+/*
+
+Fetching:
+
+fetch():
+- Metodo para iniciar el proceso de hacer una busqueda de un recurso en la web.
+recurso: cada una de las paginas que estan en el servidor.
+- Retorna una promesa(objeto que produce un valor en el futuro).
+
+Sintaxis:
+fetch(resource), -> resourse es una url.
+fetch(resource, options)
+
+Options: permiten que una conexion sea del mismo origen.
+- Method, header, body, mode, credentials.
+
+
+*/
