66bc8844f0b304b841dc7ff580509f3d32f51c15
Alfredo Sánchez <alfred3ks@gmail.com>
2022-07-24 17:53:12 +0200

    Alternativas a fetch y axios.

---
+++ modulo-6-consumiendo-apis/8-alternativas-fetch-axios.js
@@ -0,0 +1,11 @@
+/*
+
+A parte de fetch y axios existen otras alternativas:
+
+- SWR -> https://swr.vercel.app/es-ES
+- React Query -> https://tanstack.com/query/v4/?from=reactQueryV3&original=https://react-query-v3.tanstack.com/
+
+Son proyectos Open Source al igual que Axios.
+
+
+*/
