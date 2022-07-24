bfaf3d7d5bb8a144678dd94ba7fce986f635df3d
Alfredo Sánchez <alfred3ks@gmail.com>
2022-07-24 16:28:25 +0200

    Axios.

---
+++ modulo-6-consumiendo-apis/6-axios.js
@@ -0,0 +1,19 @@
+/*
+
+Vamos a ver AXIOS. Una libreria externa para hacer peticiones a una API.
+
+AXIOS:
+- Promise based HTPP client for the browser and node.js.
+- Automatic tramsforms for JSON data.
+
+SNTAXIS:
+
+axios.get(URL),
+axios.post(URL, object)
+
+Global Configuration:
+axios.defaults.baseURL = "https://api.sample.com";
+axios.defaults.headers.common['Authorization']=AUTH_TOKEN;
+axios.defaults.headers.post['Content-Type']= 'application/x-www-form-urlencoded';
+
+*/
