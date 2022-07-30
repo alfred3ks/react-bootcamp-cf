15e0df97d46ce1331f09d4e567b66b617918b900
Alfredo Sánchez <alfred3ks@gmail.com>
2022-07-30 10:50:58 +0200

    Creacion del ejercicio, limpia de archivos y instalacion de styled components.

---
+++ modulo-7-react-context/3-pagina-personal/info.js
@@ -0,0 +1,12 @@
+/*
+
+Vamos a crear una pagina personal donde pondremos en practica todo lo visto, usaremos npx create React app para este proyecto. Un generador de proyestos con React.
+
+Para entener lo que voy hacer ire creado carpetas con cada cambio generado para ver la evolucion del proyecto.
+
+Para el css vamos a usar styled component asi que instalaremos esa dependencia.
+
+npm install styled-components
+
+
+*/
